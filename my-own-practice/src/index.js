8cd9bfa06a78d730ef064ad59fc46a954c0246da
qzh <zhangq5721@gmail.com>
2017-11-28 13:46:05 +0100

    change the functional component Greeting() to be a class component

--- my-own-practice/src/index.js
+++ my-own-practice/src/index.js
@@ -10,38 +10,28 @@ function formatUser(user) {
 	return user.lastname + ', ' + user.firstname;
 }
 
-// function greeting(user) {
-// 	if(user) {
-// 		return 'Hello ' + formatUser(user) + ' !';
-// 	}
-// 	else {
-// 		return 'Hello stranger !';
-// 	}
-// }
-
-function Greeting(props) {
-	if(props.user) {
-		return (
-			<div >
-		        <h1>Hello {formatUser(props.user)} !</h1> 
-		        <h2 > It is { new Date().toLocaleTimeString() }. </h2> 
-	        </div>
-        );
-	}
-	else {
-		return (
-			<div >
-		        <h1>Hello stranger !</h1> 
-		        <h2 > It is { new Date().toLocaleTimeString() }. </h2> 
-	        </div>
-        );
+class Greeting extends React.Component {
+	render() {
+		if(this.props.user) {
+			return (
+				<div >
+			        <h1>Hello {formatUser(this.props.user)} !</h1> 
+			        <h2 > It is { new Date().toLocaleTimeString() }. </h2> 
+		        </div>
+	        );
+		}
+		else {
+			return (
+				<div >
+			        <h1>Hello stranger !</h1> 
+			        <h2 > It is { new Date().toLocaleTimeString() }. </h2> 
+		        </div>
+	        );
+		}
 	}
 }
 
-
 function tick() {
-
-
     ReactDom.render(
         <Greeting user={user} />,
         document.getElementById('root')
