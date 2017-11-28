8229ba91ed777a8d874285ef2c38e4fd32ccc5c0
qzh <zhangq5721@gmail.com>
2017-11-28 12:59:26 +0100

    combine two exmaples into one

--- my-own-practice/src/index.js
+++ my-own-practice/src/index.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// const user = {
-// 	'firstname': 'Qi',
-// 	'lastname': 'Zhang'
-// };
+const user = {
+	'firstname': 'Qi',
+	'lastname': 'Zhang'
+};
 
-// function formatUser(user) {
-// 	return user.lastname + ', ' + user.firstname;
-// }
+function formatUser(user) {
+	return user.lastname + ', ' + user.firstname;
+}
 
-// function greeting(user) {
-// 	if(user) {
-// 		return 'Hello ' + formatUser(user) + ' !';
-// 	}
-// 	else {
-// 		return 'Hello Stranger !';
-// 	}
-// }
+function greeting(user) {
+	if(user) {
+		return 'Hello ' + formatUser(user) + ' !';
+	}
+	else {
+		return 'Hello stranger !';
+	}
+}
 
 // ReactDom.render(
 // 	<h1>{greeting()}</h1>,
 // 	document.getElementById('root')
 // );
 
+
 function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDom.render(
-    element,
-    document.getElementById('root')
-  );
+    const element = ( 
+    	<div >
+	        <h1>{greeting(user)}</h1> 
+	        <h2 > It is { new Date().toLocaleTimeString() }. </h2> 
+        </div>
+    );
+
+    ReactDom.render(
+        element,
+        document.getElementById('root')
+    );
 }
 
 setInterval(tick, 1000);
