4e4c3af1b31bf504a54792f76327175fe333528d
qzh <zhangq5721@gmail.com>
2017-11-28 12:51:30 +0100

    try to update rendered elements

--- my-own-practice/src/index.js
+++ my-own-practice/src/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const user = {
-	'firstname': 'Qi',
-	'lastname': 'Zhang'
-};
+// const user = {
+// 	'firstname': 'Qi',
+// 	'lastname': 'Zhang'
+// };
 
-function formatUser(user) {
-	return user.lastname + ', ' + user.firstname;
+// function formatUser(user) {
+// 	return user.lastname + ', ' + user.firstname;
+// }
+
+// function greeting(user) {
+// 	if(user) {
+// 		return 'Hello ' + formatUser(user) + ' !';
+// 	}
+// 	else {
+// 		return 'Hello Stranger !';
+// 	}
+// }
+
+// ReactDom.render(
+// 	<h1>{greeting()}</h1>,
+// 	document.getElementById('root')
+// );
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDom.render(
+    element,
+    document.getElementById('root')
+  );
 }
 
-function greeting(user) {
-	if(user) {
-		return 'Hello ' + formatUser(user) + ' !';
-	}
-	else {
-		return 'Hello Stranger !';
-	}
-}
-
-ReactDom.render(
-	<h1>{greeting()}</h1>,
-	document.getElementById('root')
-);
-
+setInterval(tick, 1000);
