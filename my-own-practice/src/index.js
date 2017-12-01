da484057065001b71acc20fec093d80c4c83dc23
qzh <zhangq5721@gmail.com>
2017-12-01 14:13:39 +0100

    expand code to practise preventing component from rendering through the class Page and function WarningBanner

--- my-own-practice/src/index.js
+++ my-own-practice/src/index.js
@@ -89,7 +89,75 @@ function Clock(props) {
     );
 }
 
+/**
+ * It works because in JavaScript, 
+ * true && expression always evaluates to expression, 
+ * and false && expression always evaluates to false.
+ */
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+      <div>
+        <h1>Hello!</h1>
+        {unreadMessages.length > 0 &&
+          <h2>
+            You have {unreadMessages.length} unread messages.
+          </h2>
+        }
+      </div>
+    );
+  }
+  
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+function WarningBanner(props) {
+	if (!props.warn) {
+	  return null;
+	}
+  
+	return (
+	  <div className="warning">
+		Warning!
+	  </div>
+	);
+  }
+  
+  class Page extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {showWarning: true}
+	  this.handleToggleClick = this.handleToggleClick.bind(this);
+	}
+  
+	handleToggleClick() {
+	  this.setState(prevState => ({
+		showWarning: !prevState.showWarning
+	  }));
+	}
+  
+	render() {
+	  return (
+		<div>
+		  <WarningBanner warn={this.state.showWarning} />
+		  <button onClick={this.handleToggleClick}>
+			{this.state.showWarning ? 'Hide' : 'Show'}
+		  </button>
+		</div>
+	  );
+	}
+  }
+  
+function App() {
+	  return (
+		  <div>
+			  <Greeting />
+			  <Mailbox unreadMessages={messages} />
+			  <Page />
+		  </div>
+	  );
+  }
+
 ReactDom.render(
-	<Greeting />,
+	<App />,
 	document.getElementById('root')
 );
