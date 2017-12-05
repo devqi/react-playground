84911d55e5e3001da5583da7ca6976a6c6ac3cea
qzh <zhangq5721@gmail.com>
2017-12-05 11:34:06 +0100

    "initial": expand jsx and compile it

--- inital/app.js
+++ inital/app.js
@@ -1,3 +1,13 @@
-var template = <h1>This is a successfully tested JSX from app.js !</h1>;
+var template = (
+    <div>
+        <h1>successfully tested JSX</h1>
+        <p>Some info ... </p>
+        <ol>
+            <li>item one</li>
+            <li>item two</li>
+            <li>item three</li>
+        </ol>
+    </div>
+);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- inital/index.js
+++ inital/index.js
@@ -1,9 +1,37 @@
 'use strict';
 
 var template = React.createElement(
-  'h1',
-  null,
-  'This is a successfully tested JSX from app.js !'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'successfully tested JSX'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Some info ... '
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item three'
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
