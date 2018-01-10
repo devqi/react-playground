e761ccc58758f41f40d7fd7a0c34ee92a2a450a6
qzh <zhangq5721@gmail.com>
2018-01-10 15:12:07 +0100

    delete useless projects

--- inital/playground/es6-arrow-function.js
+++
@@ -1,9 +0,0 @@
-const multiplier = {
-    multiplyBy: 2,
-    bases: [1, 2, 3],
-    multiply() {
-        return this.bases.map(base => base * this.multiplyBy );
-    }
-};
-
-console.log(multiplier.multiply());
--- inital/playground/es6-let-const.js
+++
@@ -1,5 +0,0 @@
-if(3 > 0) {
-    const firstName = 'Qi';
-    console.log(firstName);
-}
-// console.log(firstName);
