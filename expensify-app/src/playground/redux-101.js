aa10e578fb5cd522f029c2db8749ef9ab2f1b954
qzh <zhangq5721@gmail.com>
2018-01-11 18:26:24 +0100

    "expensify-app": play with redux

---
+++ expensify-app/src/playground/redux-101.js
@@ -0,0 +1,34 @@
+import {createStore} from 'redux';
+
+console.log('redux-101');
+
+const store = createStore((state = { count: 10 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
+});
+
+console.log(store.getState());
+
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState());
+
+store.dispatch({type: 'DECREMENT'});
+console.log(store.getState());
+
+store.dispatch({type: 'RESET'});
+console.log(store.getState());
+
