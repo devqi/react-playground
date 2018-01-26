23fd4cf69e05a147c7d06f24bb8306f8a20f26c1
qzh <zhangq5721@gmail.com>
2018-01-26 17:18:59 +0100

    add AddExpense form (not finished)

---
+++ expensify-app/src/components/ExpenseForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: ''
+    };
+
+    onDescriptionChange = (e) => {
+        /**
+         * snippet#1: bind input with state.description
+         */
+        // this.setState({ description: e.target.value });
+
+        /**
+         * snippet#2: bind input with state.description
+         */
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    }
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState( () => ({ note }) );
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description..."
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}/>
+                    <input type="number" placeholder="Amount..."/>
+                    <textarea
+                        placeholder="Add a note for your expense (optional) ..."
+                        value={this.state.note}
+                        onChange={this.onNoteChange}/>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        );
+    }
+}
