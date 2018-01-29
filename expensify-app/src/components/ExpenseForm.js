3b8380f13083d8657414661cd4e08b3c25e89b48
qzh <zhangq5721@gmail.com>
2018-01-29 11:28:13 +0100

    add another way to change state; add onChange method for a number field

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        note: ''
+        note: '',
+        amount: ''
     };
 
     onDescriptionChange = (e) => {
@@ -15,14 +16,28 @@ export default class ExpenseForm extends React.Component {
         /**
          * snippet#2: bind input with state.description
          */
+        // e.persist(); this.setState( () => ({ description: e.target.value}));
+
+        /**
+         * snippet#3: bind input with state.description
+         */
         const description = e.target.value;
-        this.setState(() => ({ description }));
-    }
+        this.setState(() => ({description}));
+    };
 
     onNoteChange = (e) => {
         const note = e.target.value;
-        this.setState( () => ({ note }) );
-    }
+        this.setState(() => ({note}));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        // constrain the input for amount to be a number rounded to two decimal places
+        const regex = /^\d*(\.\d{0,2})?$/;
+        if(amount.match(regex)){
+            this.setState( () => ({amount}) );            
+        }
+    };
 
     render() {
         return (
@@ -33,12 +48,16 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Description..."
                         autoFocus
                         value={this.state.description}
-                        onChange={this.onDescriptionChange}/>
-                    <input type="number" placeholder="Amount..."/>
+                        onChange={this.onDescriptionChange} />
+                    <input
+                        type="number"
+                        placeholder="Amount..." 
+                        value={this.state.amount}
+                        onChange={this.onAmountChange} />
                     <textarea
                         placeholder="Add a note for your expense (optional) ..."
                         value={this.state.note}
-                        onChange={this.onNoteChange}/>
+                        onChange={this.onNoteChange} />
                     <button>Add Expense</button>
                 </form>
             </div>
